72d0b8b9e13a9c8aea01bb6d4b44d1bcc0043d49
Flysade <ata2005hh@gmail.com>
2026-01-24 10:40:44 +0100

    commit

--- script.js
+++ script.js
@@ -1025,6 +1025,7 @@ function showStartPage() {
  <section id="homepage">
  <h1 style="text-align: start" id="intro">Entwickle deine</h1> 
   <h2  style="max-width: 65%; font-size: 20px;" class="start-item"> #<span style="font-size: 20px;" id="typing-text" class="gradiant-text"></span></h2>
+<p style="color: var(--text);">kostenlos · einfach · schnell</p>
 
  <div style="display:flex; justify-content: start; align-items: start;" class="stat-cards">
         <div class="stat-card" id="screentime-box">
